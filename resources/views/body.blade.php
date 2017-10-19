cd1dd98d5b9bfc7fbe2510f672120a971adb1018
Gabe Cook <gabe565@users.noreply.github.com>
2017-10-18 19:59:35 -0500

    Remove Googlebot debug code

--- resources/views/body.blade.php
+++ resources/views/body.blade.php
@@ -36,35 +36,6 @@
 
             gtag('config', 'UA-81536605-1');
         </script>
-        <script>
-
-window.onerror = function (message, url, lineNo, colNo, error) {
-
-       console.log(arguments);
-
-          var container = document.createElement('div');
-
-             container.style.color = 'red';
-                container.style.position = 'fixed';
-                   container.style.background = '#eee';
-                      container.style.padding = '2em';
-                         container.style.top = '1em';
-                            container.style.left = '1em';
-
-                               var msg = document.createElement('pre');
-                                  msg.innerText = [
-                                            'Message: ' + message,
-                                                  'URL: ' + url,
-                                                        'Line: ' + lineNo,
-                                                              'Column: ' + colNo,
-                                                                    'Stack: ' + (error && error.stack)
-                                                                           ].join('\n');
-
-                                     container.appendChild(msg);
-
-                                        document.body.appendChild(container);
-                                        };
-        </script>
     </head>
     <body>
         <div id="app">
