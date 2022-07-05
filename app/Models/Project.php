486d7c5eb022fd221fcac1bf1267069eacab281e
Gabe Cook <gabe565@gmail.com>
2022-07-05 16:26:36 -0500

    :sparkles: Add more powerful url filter

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
     public $timestamps = false;
     protected $hidden = ['heading'];
+    protected $appends = [
+        'pretty_url',
+    ];
+
+    function prettyUrl(): Attribute {
+        return Attribute::make(
+            get: fn () => Str::of($this->url)
+                ->replaceMatches('|^(\w+:)?//|', '')
+                ->replaceMatches('|^github\.com/|', ''),
+        );
+    }
 }
