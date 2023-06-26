3d335dff0938b690ad04cf8ea3bcb32b368ccded
Gabe Cook <gabe565@gmail.com>
2023-06-26 03:31:51 -0500

    :truck: (frontend): Move router into plugins dir

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./plugins/router";
 import VueGoogleMaps from "@fawmi/vue-google-maps";
 
 import "./scss/main.scss";
