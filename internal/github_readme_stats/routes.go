7b2803323d4116f0f94eaadd3ac3a70285afd51e
Gabe Cook <gabe565@gmail.com>
2023-07-17 11:18:39 -0500

    :wrench: Allow HEAD to readme-generator endpoints

--- internal/github_readme_stats/routes.go
+++ internal/github_readme_stats/routes.go
@@ -3,6 +3,9 @@ package github_readme_stats
 import "github.com/pocketbase/pocketbase/core"
 
 func RegisterRoutes(e *core.ServeEvent) {
+	e.Router.HEAD("/api/github-stats/stats", ReadmeStatsHandler)
 	e.Router.GET("/api/github-stats/stats", ReadmeStatsHandler)
+
+	e.Router.HEAD("/api/github-stats/top-langs", ReadmeTopLangsHandler)
 	e.Router.GET("/api/github-stats/top-langs", ReadmeTopLangsHandler)
 }
