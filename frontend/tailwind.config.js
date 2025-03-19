5844924cc147dee4f5b86164d246bf36240ec901
Gabe Cook <gabe565@gmail.com>
2025-03-19 01:51:08 -0500

    chore(frontend): update styles for tailwindcss v4

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,4 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    fontFamily: {
-      display: ['"Zilla Slab"', ...defaultTheme.fontFamily.sans],
-    },
-  },
 };
